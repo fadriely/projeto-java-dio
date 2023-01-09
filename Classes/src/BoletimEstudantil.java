7c5e802af564f909489f97aebd815ccfc6fa4769
Adriely Ferreira <ferreiraadriely02@gmail.com>
2023-01-09 16:43:15 -0400

    Update BoletimEstudantil.java

--- Classes/src/BoletimEstudantil.java
+++ Classes/src/BoletimEstudantil.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BoletimEstudantil {
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
 }

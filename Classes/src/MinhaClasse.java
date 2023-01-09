3d3ae444cc568b0c8d9a3e9de174d58ceb470e47
Adriely Ferreira <ferreiraadriely02@gmail.com>
2023-01-09 16:44:02 -0400

    Update MinhaClasse.java

--- Classes/src/MinhaClasse.java
+++ Classes/src/MinhaClasse.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MinhaClasse {
+public class MinhaClasse {
+
+    public static void main(String [] args) {
+
+        String primeiroNome = "Adriely";
+        String segundoNome="Ferreira";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }

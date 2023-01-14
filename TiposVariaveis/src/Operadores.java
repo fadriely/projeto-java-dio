93a1328356b03815c099a6964b8e95161d21f450
fadriely <ferreiraadriely02@gmail.com>
2023-01-13 22:27:06 -0400

    commitando a pasta de TiposVariáveis

---
+++ TiposVariaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Adriely";
+        String segundoNome = " Pereira Ferreira";
+
+        String nomeCompleto = primeiroNome + segundoNome;
+
+        System.out.println(nomeCompleto);
+
+
+    }
+}
+
+
+
---
+++ TiposVariaveis/src/OperadoresConcatenacao.java
@@ -0,0 +1,27 @@
+public class OperadoresConcatenacao {
+    public static void main(String[] args) {
+
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+    }
+}

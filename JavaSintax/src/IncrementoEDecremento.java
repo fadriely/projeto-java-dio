bce88dd1d301bf8440d70c959f0bbf5c1acc45df
fadriely <ferreiraadriely02@gmail.com>
2023-01-17 21:25:32 -0400

    commitando pastas

---
+++ JavaSintax/src/IncrementoEDecremento.java
@@ -0,0 +1,14 @@
+public class IncrementoEDecremento {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //numero = numero + 1;
+
+        //System.out.println( -- numero );
+
+        System.out.println( ++ numero); //incrementar e decrementar valores em 1
+
+        System.out.println( numero );
+    }
+}
+    
